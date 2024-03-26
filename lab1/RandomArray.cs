690bac34831cc8a50b3eceb16b50e3a38da4d8c5
Jim Sjöberg <jim.sj18@gmail.com>
2024-03-26 16:41:20 +0100

    class for creating random array

---
+++ lab1/RandomArray.cs
@@ -0,0 +1,20 @@
+﻿namespace lab1;
+using System;
+
+//Generate a array of random numbers
+//array size depens in funktion input 
+public class RandomArray
+{
+    public static int[] Create(int iterations)
+    {
+        Random randomGenerator = new Random();
+        int[] randomArray= new int[iterations];
+        
+        for (int i = 0; i < iterations; i++)
+        {
+            randomArray[i] = randomGenerator.Next(1, iterations);
+        }
+        
+        return randomArray;
+    }
+}
